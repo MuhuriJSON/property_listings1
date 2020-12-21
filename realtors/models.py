9dce04962b83dbe276ca50543b910875d4d4a5d9
muhurijson <muhuri.json@gmail.com>
2020-12-21 17:46:47 +0300

    changed realtor phonefield maxlength

--- realtors/models.py
+++ realtors/models.py
@@ -10,7 +10,7 @@ class Realtor(models.Model):
 	name = models.CharField(max_length=200)
 	photo = models.ImageField(upload_to='photo/%Y/%m/%d', blank=True)
 	description = models.TextField(blank=True)
-	phone = models.CharField(max_length=10)
+	phone = models.CharField(max_length=50)
 	email = models.CharField(max_length=50)
 	is_mvr = models.BooleanField(default=False, blank=True)
 	hire_date = models.DateTimeField(default=datetime.now, blank=True)
