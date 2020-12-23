5526018941763206ee56398c45ef96197581461b
muhurijson <muhuri.json@gmail.com>
2020-12-23 13:58:30 +0300

    changed realtor field in listing model

--- listings/models.py
+++ listings/models.py
@@ -8,7 +8,7 @@ from realtors.models import Realtor
 class Listing(models.Model):
 
     user_id = models.IntegerField()
-    realtor = models.ForeignKey(Realtor, on_delete=models.CASCADE)
+    realtor = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     town = models.CharField(max_length=30, blank=True)
     rent_or_sale = models.CharField(max_length=4, blank=True, default='Rent')
